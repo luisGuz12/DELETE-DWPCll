4fedcb0db82b678813a18e91d1a6d50aa8eebbc2
guzman jimenez luis alfonso <124893198+luisGuz12@users.noreply.github.com>
2023-10-02 17:15:27 +0000

    ⭐feauter: prettier & eslint

--- server/debugLogger.js
+++ server/debugLogger.js
@@ -2,6 +2,6 @@
 // Importando dependencia debug
 import Debug from 'debug'
 // Establecemos el proceso a debuggear
-const debug = Debug('dwpcii:server')
+const debug = Debug('dwpcll:server')
 // exportando el logger de debuggeo
 export default debug
