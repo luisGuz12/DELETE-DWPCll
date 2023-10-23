d2976c576bc95947a8bca0151e679a422a44b766
guzman jimenez luis alfonso <124893198+luisGuz12@users.noreply.github.com>
2023-10-23 16:47:14 +0000

    🎯 S03E02 Rutas de acciones

---
+++ server/domains/project/project.controller.js
@@ -0,0 +1,28 @@
+// Action Methods
+
+// Get '/project/projects'
+const projects = (req, res) => {
+  res.send("🚧 UNDER CONSTRUCTION GET '/project/projects'🚧");
+};
+
+// Get '/project/dashboard'
+const dashboard = (req, res) => {
+  res.send("🚧 UNDER CONSTRUCTION GET '/project'🚧");
+};
+
+// Get '/project/projects'
+const addform = (req, res) => {
+  res.send("🚧 UNDER CONSTRUCTION GET '/project/add-form'🚧");
+};
+
+// Get '/project/add'
+const add = (req, res) => {
+  res.send("🚧 UNDER CONSTRUCTION GET '/project/add'🚧");
+};
+
+export default {
+  projects,
+  dashboard,
+  addform,
+  add,
+};
