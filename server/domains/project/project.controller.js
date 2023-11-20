92dff6ff9404ce205b1c45241297791b73cb6d28
guzman jimenez luis alfonso <124893198+luisGuz12@users.noreply.github.com>
2023-11-20 22:48:41 +0000

    📸☀ S04A06 Mensajes Flash

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -54,6 +54,8 @@ const addPost = async (req, res) => {
     log.info(`Se carga proyecto ${savedProject}`);
     // Se registra en el log el redireccionamiento
     log.info('Se redirecciona el sistema a /project');
+    // Agregando mensaje de flash
+    req.flash('successMessage', 'Proyecto agregado con exito');
     // Se redirecciona el sistema a la ruta '/project'
     return res.redirect('/project');
   } catch (error) {
@@ -126,6 +128,8 @@ const editPut = async (req, res) => {
     // Se salvan los cambios
     log.info(`Actualizando proyecto con id: ${id}`);
     await project.save();
+    // Generando mensaje FLASH
+    req.flash('successMessage', 'Proyecto editado con exito');
     return res.redirect(`/project/edit/${id}`);
   } catch (error) {
     log.error(`Error al actualizar proyecto con id: ${id}`);
@@ -134,7 +138,19 @@ const editPut = async (req, res) => {
 };
 
 // DELETE "/project/:id"
-const deleteProject = (req, res) => res.status(200).json({ id: req.params.id });
+const deleteProject = async (req, res) => {
+  // Extrayendo el id de los parametros
+  const { id } = req.params;
+  // Usando el modelo para borrar el proyecto
+  try {
+    const result = await ProjectModel.findByIdAndRemove(id);
+    // Agregando mensaje de flash
+    req.flash('successMessage', 'Proyecto borrado con exito');
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
 
 export default {
   // Action Methods
